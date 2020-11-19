463fe07f336cbda93372d935c133015e23ba719f
NicholasZanardi <nicholaszanardi108@gmail.com>
2020-11-19 12:04:26 +0700

    Up Kitchen

--- presentation/kitchen/view/ListBahan.php
+++ presentation/kitchen/view/ListBahan.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#">Navbar</a>
+			<a class="navbar-brand" href="#">Kitchen</a>
 		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		    	<span class="navbar-toggler-icon"></span>
 		  	</button>
@@ -15,10 +15,7 @@
 		        		<a class="nav-link" href="ListMenu.php">Menu</a>
 		      		</li>
 		      		<li class="nav-item  active">
-		        		<a class="nav-link" href="">Nama<span class="sr-only">(current)</span></a>
-		      		</li>
-		      		<li class="nav-item">
-		        		<a class="nav-link" href="#">Add Menu</a>
+		        		<a class="nav-link" href="ListBahan.php">Bahan<span class="sr-only">(current)</span></a>
 		      		</li>
 		    	</ul>
 		  	</div>
--- presentation/kitchen/view/ListMenu.php
+++ presentation/kitchen/view/ListMenu.php
@@ -5,20 +5,17 @@
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#">Navbar</a>
+			<a class="navbar-brand" href="#">Kitchen</a>
 		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		    	<span class="navbar-toggler-icon"></span>
 		  	</button>
 		  	<div class="collapse navbar-collapse" id="navbarNav">
 		    	<ul class="navbar-nav">
-		      		<li class="nav-item">
-		        		<a class="nav-link" href="ListBahan.php">List Bahan</a>
-		      		</li>
-		      		<li class="nav-item  active">
-		        		<a class="nav-link" href="">Nama<span class="sr-only">(current)</span></a>
+		      		<li class="nav-item active">
+		        		<a class="nav-link" href="ListMenu.php">Menu<span class="sr-only">(current)</span></a>
 		      		</li>
 		      		<li class="nav-item">
-		        		<a class="nav-link" href="#">Add Bahan</a>
+		        		<a class="nav-link" href="ListBahan.php">Bahan</a>
 		      		</li>
 		    	</ul>
 		  	</div>
