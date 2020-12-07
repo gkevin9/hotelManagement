2d00f25e1f61b54691ce2c4e0c0660b360d524df
gkevin9 <georgius98@gmail.com>
2020-12-07 19:27:21 +0700

    percobaan
    ditambah class

--- presentation/receptionist/controller/AvaliableRoomController.php
+++ presentation/receptionist/controller/AvaliableRoomController.php
@@ -9,18 +9,43 @@ $checkin = $_POST['checkin'];
 $checkout = $_POST['checkout'];
 $person = $_POST['person'];
 
-if($checkin >= $checkout) {
-    print json_encode("wrongCheckinDate");
-}else {
-    $ctrl = new ServiceReservation\RoomAvaliabilityService();
-    $avaliableRoomArray = $ctrl->getAvaliableRoomFromDate($checkin, $checkout, $person);
+// if($checkin >= $checkout) {
+//     print json_encode("wrongCheckinDate");
+// }else {
+//     $ctrl = new ServiceReservation\RoomAvaliabilityService();
+//     $avaliableRoomArray = $ctrl->getAvaliableRoomFromDate($checkin, $checkout, $person);
     
-    if(empty($avaliableRoomArray)) {
-        print json_encode("noRoom");
-    }else {
-        print json_encode($avaliableRoomArray);
+//     if(empty($avaliableRoomArray)) {
+//         print json_encode("noRoom");
+//     }else {
+//         print json_encode($avaliableRoomArray);
+//     }
+    
+// }
+
+$ctrl = new AvaliableRoomController();
+$result = $ctrl->getAvaliableRoom();
+print json_encode($result)
+
+class AvaliableRoomController {
+    public function getAvaliableRoom($checkin, $checkout, $person) {
+        if($this->validateDate($checkin, $checkout)){
+            return "wrongCheckinDate";
+        }else {
+            $ctrl = new ServiceReservation\RoomAvaliabilityService();
+            $avaliableRoomArray = $ctrl->getAvaliableRoomFromDate($checkin, $checkout, $person);
+            
+            if(empty($avaliableRoomArray)) {
+                return "noRoom";
+            }else {
+                return $avaliableRoomArray;
+            }
+        }
     }
-    
+
+    private function validateDate($checkin, $checkout) {
+        return $checkin >= $checkout;
+    }
 }
 
 ?>
--- presentation/receptionist/controller/CustomerCheckinController.php
+++ presentation/receptionist/controller/CustomerCheckinController.php
@@ -10,23 +10,56 @@ require_once($_SERVER['DOCUMENT_ROOT']."/domain/guest/model/NewBillModel.php");
 use domain\guest\service as Service;
 use domain\guest\model as Model;
 
+// $id = $_GET['id'];
+
+// $service = new Service\CustomerStayService();
+// $service->newCustomerStay($id);
+
+// $lama = $service->getLama($id);
+// $amount = $service->getUangMuka($id);
+
+// $billModel = new Model\NewBillModel();
+// $billModel->setAmount($amount);
+// $billModel->setQuantity($lama);
+// $billModel->setDeskripsi("Biaya Kamar");
+// $billModel->setCustStay($id);
+// echo $amount;
+
+// $billService = new Service\BillService();
+// $billService->createBill($billModel);
+
+// header("Location: ../view/ListReservation.php");
 $id = $_GET['id'];
-
-$service = new Service\CustomerStayService();
-$service->newCustomerStay($id);
-
-$lama = $service->getLama($id);
-$amount = $service->getUangMuka($id);
-
-$billModel = new Model\NewBillModel();
-$billModel->setAmount($amount);
-$billModel->setQuantity($lama);
-$billModel->setDeskripsi("Biaya Kamar");
-$billModel->setCustStay($id);
-echo $amount;
-
-$billService = new Service\BillService();
-$billService->createBill($billModel);
-
+$ctrl = new CustomerCheckinController();
+$ctrl->customerCheckin($id);
 header("Location: ../view/ListReservation.php");
+
+class CustomerCheckinController {
+    public function customerCheckin($id) {
+        $this->newCustomerStay($id);
+        $this->newBill($id);
+    }
+
+    private function newCustomerStay($id) {
+        $service = new Service\CustomerStayService();
+        $service->newCustomerStay($id);
+    }
+
+    private function newBill($id) {
+        $service = new Service\CustomerStayService();
+
+        $lama = $service->getLama($id);
+        $amount = $service->getUangMuka($id);
+
+        $billModel = new Model\NewBillModel();
+        $billModel->setAmount($amount);
+        $billModel->setQuantity($lama);
+        $billModel->setDeskripsi("Biaya Kamar");
+        $billModel->setCustStay($id);
+        echo $amount;
+
+        $billService = new Service\BillService();
+        $billService->createBill($billModel);
+    }
+}
 ?>
